9b9818fef90376a7f259258ff7a09c2302ba012e
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-04-24 20:31:15 +0200

    resgiter user completed

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,22 +1,19 @@
 import express from 'express';
 import User from "../models/user.js";
 import bcrypt from 'bcrypt';
+import mongoose from 'mongoose'
 const userRouter = express.Router();
 
-const users = {
-    name: 'lars',
-    password: 123123
-}
 
 userRouter.get('/loggedIn', (req, res) => {
 
-    res.send(users);
-    // if(req.session){
-    //     res.status(200).send(req.session.username)
+   
+    if(req.session){
+        res.status(200).send(req.session.username)
         
-    // } else {
-    //     res.status(401).send('Nobody has logged in');
-    // }
+    } else {
+        res.status(401).send('Nobody has logged in');
+    }
 
 })
 
@@ -31,7 +28,7 @@ userRouter.get('/loggedIn/role', (req, res) => {
 
 userRouter.post('/register', (req, res) => {
 
-    const { name, password } = req.body;
+    const { userName, password } = req.body;
     const existingUsers = User.find({userName});
 
     for(let i = 0; i < existingUsers.length; i++){
@@ -42,12 +39,12 @@ userRouter.post('/register', (req, res) => {
 
     const hashedPassword = bcrypt.hash(password, 10);
 
-    // if(passWord !== req.body.rePassword){
+    // if(password !== req.body.rePassword){
     //     return res.status(406).json('Password was incorrect')
     // }
 
     const newUser = new User({
-      _id: mongoose.Schema.Types.ObjectId(),
+      _id: mongoose.Types.ObjectId(),
       userName: userName,
       password: hashedPassword,
       role: 'user',
